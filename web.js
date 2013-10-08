1de8daffc2f6a955e896ae088654b909d4e9eb17
Yang Li <idealhack@gmail.com>
2013-10-08 22:04:51 +0800

    directly require() json file

--- web.js
+++ web.js
@@ -1,9 +1,7 @@
 var express = require('express');
+var quotes = require('./quotes.json');
 
 var app = express.createServer(express.logger());
-
-var data = require('fs').readFileSync(__dirname + '/quotes.json', 'utf8');
-var quotes = JSON.parse(data);
 
 app.get('/', function(req, res) {
   var random = Math.floor(Math.random() * quotes.length);
