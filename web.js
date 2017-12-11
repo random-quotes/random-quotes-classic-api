160a279310d4e1029e9c583762d6e1f06dde2996
Yang Li <idealhack@gmail.com>
2017-12-11 16:00:29 +0800

    upgrade express to 4.x

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var logger = require('morgan');
 var yaml = require('js-yaml');
 var fs = require('fs');
 
 var quotes = yaml.load(fs.readFileSync('quotes.yaml'));
 var app = express();
 
-app.use(express.logger());
+app.use(logger('combined'))
 
 app.get('/', function (req, res) {
   var random = Math.floor(Math.random() * quotes.length);
