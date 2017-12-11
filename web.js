aadd1710f4c0e02640ee8c5a4091cf5fe92b8f15
Yang Li <idealhack@gmail.com>
2017-12-11 15:42:34 +0800

    use yaml instead of json

--- web.js
+++ web.js
@@ -1,17 +1,19 @@
 var express = require('express');
-var quotes = require('./quotes.json');
+var yaml = require('js-yaml');
+var fs = require('fs');
 
+var quotes = yaml.load(fs.readFileSync('quotes.yaml'));
 var app = express();
 
 app.use(express.logger());
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   var random = Math.floor(Math.random() * quotes.length);
   res.header('Access-Control-Allow-Origin', '*');
   res.json(quotes[random]);
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Listening on " + port);
 });
